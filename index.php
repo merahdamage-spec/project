1559504f2762f79fef176bc904a2419a65c7d74e
merahdamage-spec <merahdamage@gmail.com>
2025-11-07 11:18:25 +0700

    hapu a by DIA

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-a
+
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap');
 
@@ -394,3 +394,4 @@ $result = $conn->query("SELECT * FROM layanan");
  
 </body>
 </html>
+
